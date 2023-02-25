0b66051ae91b5f9aee4a3fd956efad0b585c0530
Gadzhi <62199271+Gadzhi07@users.noreply.github.com>
2023-02-25 14:51:29 +0300

    Update mailsender.py

--- mailsender/mailsender.py
+++ mailsender/mailsender.py
@@ -6,8 +6,6 @@ f = open("TaskExec.log", "r")
 reply_but, send_mail_but = None, None
 del_but, ready_del_but = None, None
 
-count = None
-
 try:
 	lines = f.readlines()
 	sleep(5)
@@ -21,22 +19,16 @@ try:
 			del_but = pyautogui.center(del_but)
 		mail_hacked = line.strip().split(" ")
 		print(mail_hacked)
-		mail_num = int(mail_hacked[0])
-		if not count:
-			count = mail_num
 		password = mail_hacked[4]
-		if mail_num < count:
-			exit("TaskExec.log is invalid")
-		if count == mail_num:
-			pyautogui.click(reply_but)
-			sleep(0.2)
-			pyautogui.write(password)
-			sleep(0.6)
-			if not send_mail_but:
-				send_mail_but = pyautogui.locateOnScreen("send_mail_but.png")
-				send_mail_but = pyautogui.center(send_mail_but)
-			pyautogui.click(send_mail_but)
-			count += 1
+		pyautogui.click(reply_but)
+		sleep(0.2)
+		pyautogui.write(password)
+		sleep(0.6)
+		if not send_mail_but:
+			send_mail_but = pyautogui.locateOnScreen("send_mail_but.png")
+			send_mail_but = pyautogui.center(send_mail_but)
+		pyautogui.click(send_mail_but)
+		sleep(0.5)
 		pyautogui.click(del_but)
 		sleep(0.5)
 		if not ready_del_but:
